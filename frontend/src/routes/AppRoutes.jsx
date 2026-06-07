ce6141bf92c50965ff00baf151785a0d7a77ced1
ronit1161 <ronittambe116@gmail.com>
2026-06-07 14:44:50 +0530

    Added profile page, profile edit page and freelancer dashboard

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,20 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "../pages/Auth/Login";
 import RootLayout from "../components/layout/RootLayout";
 import CreateGig from "../pages/freelancer/CreateGig";
 import FreelancerProfile from "../pages/freelancer/FreelancerProfile";
+import FreelancerDashboard from "../pages/freelancer/FreelancerDashboard";
+import EditProfile from "../pages/freelancer/EditProfile";
 export default function AppRoutes(){
     return(
         <Routes>
-            <Route path="/login" element={<Login />} />
 
             <Route path="/" element={<RootLayout />}>
-                <Route index element={<h1>Home Page</h1>} />
+                <Route index element={ <FreelancerDashboard /> } />
                 <Route path="create-gig" element={<CreateGig />} />
                 <Route path="profile" element={<FreelancerProfile />} />
+                <Route path="dashboard" element={<FreelancerDashboard />} />
+                <Route path="edit-profile" element={<EditProfile />} />
             </Route>
 
         </Routes>
     )
 }
-
