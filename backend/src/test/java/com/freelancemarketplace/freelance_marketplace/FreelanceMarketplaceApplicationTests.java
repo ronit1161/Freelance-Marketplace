5be3f0c9b8fde097a956691289f81cab3c0aabae
Nimish-Bhalerao <nimishbhalerao123@gmail.com>
2026-07-27 17:38:09 +0530

    Added spring boot backend

---
+++ backend/src/test/java/com/freelancemarketplace/freelance_marketplace/FreelanceMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelancemarketplace.freelance_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
