64eff1520f294af3d903ffea22e3397398f94c77
Nimish-Bhalerao <nimishbhalerao123@gmail.com>
2026-07-28 09:47:05 +0530

    User roles enum updated

--- backend/src/main/java/enums/UserRoles.java
+++ backend/src/main/java/enums/UserRoles.java
@@ -1,17 +1,6 @@
 package enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public enum UserRoles {
-	ADMIN,CLIENT,FREELANCER;
-	@JsonCreator
-    public static UserRoles fromString(String value) {
-        if (value == null) return null;
-        for (UserRoles role : UserRoles.values()) {
-            if (role.name().equalsIgnoreCase(value)) {
-                return role;
-            }
-        }
-        throw new IllegalArgumentException("Invalid role: " + value);
-    }
+    ADMIN, CLIENT, FREELANCER;
+
 }
