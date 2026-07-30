c7abf25de7fd46f3330ee1b58bce9e9b6fdfbc79
utkarshpatil110-png <utkarshpatil110@gmail.com>
2026-07-30 19:20:37 +0530

    revised categories and created reviews apis

--- backend/src/main/java/com/freelancemarketplace/modules/category/entity/Category.java
+++ backend/src/main/java/com/freelancemarketplace/modules/category/entity/Category.java
@@ -1,47 +1,23 @@
-
-
 package com.freelancemarketplace.modules.category.entity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.freelancemarketplace.common.entity.BaseEntity;
 
 import jakarta.persistence.AttributeOverride;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Setter
 @Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "categories")
-@AttributeOverride(name="id",column = @Column(name="category_id"))
+@AttributeOverride(name = "id", column = @Column(name = "category_id"))
 public class Category extends BaseEntity {
-	@Column(name="category_name",length = 30,nullable = false)
-	private String categoryName;
-	@Column(name="category_slug",nullable = false,unique = true)
-	private String categorySlug;//URL safe identifier
-	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    private Category parentCategory;
 
-    // 2. The Parent Side: ONE category has MANY subcategories
-	@OneToMany(
-		    mappedBy = "parentCategory",
-		    cascade = CascadeType.ALL,
-		    orphanRemoval = true,
-		    fetch = FetchType.LAZY 
-		)
-    private List<Category> subCategories = new ArrayList<>();
-	
+    @Column(name = "category_name", length = 30, nullable = false)
+    private String categoryName;
 }
